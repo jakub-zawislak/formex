8b13d8bba1290ad81bbe0bc09c7c538803853795
Jakub Zawiślak <harry19911@gmail.com>
2017-01-22 12:29:23 +0100

    mix desc

--- mix.exs
+++ mix.exs
@@ -10,24 +10,23 @@ defmodule Formex.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:ecto, "~> 2.1"}]
+  end
+
+  defp description do
+    """
+    Formex is an abstract layer that helps to build forms in Phoenix and Ecto
+    """
+  end
+
+  defp package do
+    [maintainers: ["Jakub Zawiślak"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/jakub-zawislak/formex"}]
   end
 end
