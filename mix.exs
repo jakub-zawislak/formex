e8ec97f1afca4287275bed9642d9bdc473921449
Jakub Zawiślak <harry19911@gmail.com>
2017-04-09 19:05:20 +0200

    compiled JS

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Formex.Mixfile do
 
   def project do
     [app: :formex,
-     version: "0.4.4",
+     version: "0.4.5",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
